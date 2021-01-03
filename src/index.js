540165f56673bc897789b84eae407a796433fbb9
Marc Pampols <mpampols@gmail.com>
2021-01-03 18:44:17 +0100

    Changes

--- src/index.js
+++ src/index.js
@@ -10,10 +10,15 @@ const Hello = () => {
 }
 
 const App = () => {
+  const now = new Date()
+  const a = 10
+  const b = 20
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello />
+      <p>Hello world, it is {now.toString()}</p>
+      <p>
+        {a} plus {b} is {a + b}
+      </p>
     </div>
   )
 }
